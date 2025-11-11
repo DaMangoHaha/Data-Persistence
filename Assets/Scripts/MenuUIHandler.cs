b197d1db0cf3592ff9d8e83f9f1899b8292d1bcb
Stephen Mangum <stephenmangum01@gmail.com>
2025-11-11 08:53:16 -0600

    webglk

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -25,12 +25,23 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         // this will call the NewColorSelected function when the color picker has a color button clicked
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
     }
 
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
 
 
     public void Exit()
     {
+        MainManager.Instance.SaveColor();
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
